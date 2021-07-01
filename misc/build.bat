12c58b915fcb02dca7272a1ac20b8e9cbf12e683
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2021-07-02 00:08:18 +0900

    completed Day002

--- misc/build.bat
+++ misc/build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir z:\handmade_game\project\build
 pushd z:\handmade_game\project\build
-cl -Zi z:\handmade_game\project\code\main.cpp User32.lib
+cl -Zi z:\handmade_game\project\code\main.cpp User32.lib Gdi32.lib
 popd
--- project/code/main.cpp
+++ project/code/main.cpp
@@ -1,6 +1,93 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
-  MessageBoxA(0, "This is your second message box!", "Your Window vol.2", MB_OK|MB_ICONEXCLAMATION);
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
+
+  LRESULT Result = 0;
+
+  switch(Message){
+    case WM_SIZE:{
+      OutputDebugStringA("WM_SIZE\n");
+      break;
+    }
+    case WM_DESTROY:{
+      OutputDebugStringA("WM_DESTROY\n");
+      break;
+    }
+    case WM_CLOSE:{
+      OutputDebugStringA("WM_CLOSE\n");
+      break;
+    }
+    case WM_ACTIVATEAPP:{
+      OutputDebugStringA("WM_ACTIVATEAPP\n");
+      break;
+    }
+    case WM_PAINT:{
+      PAINTSTRUCT Paint;
+      HDC DeviceContext = BeginPaint(Window, &Paint);
+      int X = Paint.rcPaint.left;
+      int Y = Paint.rcPaint.top;
+      int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+      int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+      PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+      EndPaint(Window, &Paint);
+
+      break;
+    }
+    default:
+    {
+      // OutputDebugStringA("defaut\n");
+      Result = DefWindowProc(Window, Message, WParam, LParam);
+      break;
+    }
+  }
+  return(Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode){
+  WNDCLASS WindowClass = {};
+  //TODO: Check if CS_OWNDC, CS_HREDRAW, CS_VREDRAW still matter
+  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // WindowClass.hIcon;
+  // LPCSTR    lpszMenuName;
+  WindowClass.lpszClassName = "HandmadeGameWindowClass";
+  if (RegisterClass(&WindowClass)){
+    HWND WindowHandle = CreateWindowEx(
+      0,
+      WindowClass.lpszClassName,
+      "HandmadeGameWindowClass",
+      WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+      CW_USEDEFAULT,//x
+      CW_USEDEFAULT,//y
+      CW_USEDEFAULT,//width
+      CW_USEDEFAULT,//height
+      0,
+      0,
+      Instance,
+      0);
+      if (WindowHandle) {
+          //succeeded creating window
+          for (;;){
+            MSG Message;
+            BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+            if (MessageResult > 0){
+              TranslateMessage(&Message);
+              DispatchMessage(&Message);
+            }
+            else {
+              break;
+            }
+          }
+
+      }
+      else {
+        //TODO: Logging
+      }
+  }
+  else {
+    //TODO: Logging
+  }
+
   return(0);
 }
