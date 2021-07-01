9c875c23bf8f83fde6a74ef3f5e7b307ebe43f70
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2021-07-01 21:50:00 +0900

    gitignore still not working

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
-  MessageBoxA(0, "This is your very first message box!", "Your Window", MB_YESNO|MB_ICONINFORMATION);
+  MessageBoxA(0, "This is your very first message box!", "Your Window vol.2", MB_YESNO|MB_ICONINFORMATION);
   return(0);
 }
