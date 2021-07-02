ef6969ed4de01daab972a49cb0f01cf9a2b72854
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2021-07-02 14:48:18 +0900

    Completed Day003 - Allocating a buffer

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -1,20 +1,73 @@
 #include <windows.h>
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
+#define internal static
+#define local_persist static
+#define global_variable static
+
+//TODO: This is a global for now
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height){
+  //TODO: Bulletproof this
+  // maybe dont free first, free after, then free first if that fails
+  if (BitmapHandle){
+    DeleteObject(BitmapHandle);
+  }
+  if (!BitmapDeviceContext) {
+    //TODO: Should we recreate these under certain specion circumstances
+    BitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  //TODO: Free our DIBSection
+  BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+  BitmapInfo.bmiHeader.biWidth = Width;
+  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biPlanes = 1;
+  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biCompression = BI_RGB;
+  BitmapInfo.bmiHeader.biSizeImage = 0;
+  BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+  BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+  BitmapInfo.bmiHeader.biClrUsed = 0;
+  BitmapInfo.bmiHeader.biClrImportant = 0;
+
+  BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height){
+  StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height,
+    BitmapMemory,
+    &BitmapInfo,
+    DIB_RGB_COLORS,
+    SRCCOPY);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
 
   LRESULT Result = 0;
 
   switch(Message){
     case WM_SIZE:{
-      OutputDebugStringA("WM_SIZE\n");
+      RECT ClientRect;
+      GetClientRect(Window, &ClientRect);
+      int Width = ClientRect.right - ClientRect.left;
+      int Height = ClientRect.bottom - ClientRect.top;
+      Win32ResizeDIBSection(Width, Height);
       break;
     }
     case WM_DESTROY:{
-      OutputDebugStringA("WM_DESTROY\n");
+      //TODO: Handle this as an error - recreate window?
+      Running = false;
       break;
     }
     case WM_CLOSE:{
-      OutputDebugStringA("WM_CLOSE\n");
+      //TODO: Handle this with a message to the user?
+      Running = false;
       break;
     }
     case WM_ACTIVATEAPP:{
@@ -28,7 +81,8 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LP
       int Y = Paint.rcPaint.top;
       int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
       int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-      PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+      Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
       EndPaint(Window, &Paint);
 
       break;
@@ -46,8 +100,8 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LP
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode){
   WNDCLASS WindowClass = {};
   //TODO: Check if CS_OWNDC, CS_HREDRAW, CS_VREDRAW still matter
-  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.style = CS_OWNDC;
+  WindowClass.lpfnWndProc = Win32MainWindowCallback;
   WindowClass.hInstance = Instance;
   // WindowClass.hIcon;
   // LPCSTR    lpszMenuName;
@@ -68,7 +122,9 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
       0);
       if (WindowHandle) {
           //succeeded creating window
-          for (;;){
+          Running = true;
+
+          while (Running){
             MSG Message;
             BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
             if (MessageResult > 0){
@@ -79,7 +135,6 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
               break;
             }
           }
-
       }
       else {
         //TODO: Logging
